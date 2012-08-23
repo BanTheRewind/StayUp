1dfbe68c5574694c51c4ebdf15b46f1b4e045c6e
bantherewind <bantherewind@gmail.com>
2012-08-23 16:38:17 -0400

    Added license

--- StayUp/Properties/AssemblyInfo.cs
+++ StayUp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StayUp")]
+[assembly: AssemblyTitle("Stay Up")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Fashionbuddha")]
-[assembly: AssemblyProduct("StayUp")]
-[assembly: AssemblyCopyright("Copyright © Fashionbuddha 2011")]
+[assembly: AssemblyCompany("Ban the Rewind")]
+[assembly: AssemblyProduct("Stay Up")]
+[assembly: AssemblyCopyright("Copyright © Ban the Rewind 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
