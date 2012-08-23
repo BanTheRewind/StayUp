a14ebfc0889549f256f4dfe7eb9c3f8c1ab58b87
bantherewind <bantherewind@gmail.com>
2012-08-23 16:42:20 -0400

    Updated properties

--- StayUp/Properties/AssemblyInfo.cs
+++ StayUp/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Stay Up")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ban the Rewind")]
-[assembly: AssemblyProduct("Stay Up")]
-[assembly: AssemblyCopyright("Copyright © Ban the Rewind 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle( "StayUp" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Ban the Rewind" )]
+[assembly: AssemblyProduct( "StayUp" )]
+[assembly: AssemblyCopyright( "Copyright © Ban the Rewind 2012" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ef49ccc4-3890-429f-a4cf-e5e98ac47fee")]
+[assembly: Guid( "ef49ccc4-3890-429f-a4cf-e5e98ac47fee" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion( "1.1.0.0" )]
+[assembly: AssemblyFileVersion( "1.1.0.0" )]
